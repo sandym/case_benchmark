f99083dd24b9f8580a1f060cde76ee46e874a573
sandy <sandy@mac.com>
2022-12-15 21:50:17 +1100

    remove strcasecmp

--- using_loop_if.h
+++ using_loop_if.h
@@ -15,35 +15,4 @@ inline std::string &tolower_loop_if( std::string &s )
 	return s;
 }
 
-inline int StrCaseCmpIf(std::string_view lhs, std::string_view rhs)
-{
-  auto ulhs = reinterpret_cast<const unsigned char*>(lhs.data());
-  auto urhs = reinterpret_cast<const unsigned char*>(rhs.data());
-	auto len = std::min(lhs.size(), rhs.size());
-	size_t i = 0;
-
-	auto TOLOWER = [](unsigned  char c ) -> unsigned char
-	{
-		return ( c >= 'A' and c <= 'Z' ) ? c + ' ' : c;
-	};
-  for (; i < len; i++) {
-    const int diff =
-        TOLOWER(ulhs[i]) -
-        TOLOWER(urhs[i]);
-    if (diff != 0) return diff;
-  }
-  if (i > lhs.size())
-  {
-	if (i > rhs.size())
-		return 0;
-	return TOLOWER(urhs[i]);
-  }
-  if (i > rhs.size())
-  {
-	return -TOLOWER(ulhs[i]);
-  }
-  return TOLOWER(ulhs[i]) -
-        TOLOWER(urhs[i]);
-}
-
 #endif
